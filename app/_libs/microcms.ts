578036638e12d7c4d0bd23e61334b838d7c231a1
makoto-hoshina <0798265@mchcgr.com>
2024-10-29 16:27:07 +0900

    try fixed .envValues for Versel deploy2

--- app/_libs/microcms.ts
+++ app/_libs/microcms.ts
@@ -27,13 +27,13 @@ export type News = {
 console.log(process.env.MICROCMS_API_KEY);
 console.log(process.env.MICROCMS_SERVICE_DOMAIN);
 
-if (!process.env.MICROCMS_SERVICE_DOMAIN) {
-  throw new Error("MICROCMS_SERVICE_DOMAIN is required");
-}
+// if (!process.env.MICROCMS_SERVICE_DOMAIN) {
+//   throw new Error("MICROCMS_SERVICE_DOMAIN is required");
+// }
 
-if (!process.env.MICROCMS_API_KEY) {
-  throw new Error("MICROCMS_API_KEY is required");
-}
+// if (!process.env.MICROCMS_API_KEY) {
+//   throw new Error("MICROCMS_API_KEY is required");
+// }
 
 // const client = createClient({
 //   serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
