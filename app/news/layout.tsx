aa12d9fd0c005a252e7d383d51baffeb5238bbba
makoto-hoshina <0798265@mchcgr.com>
2024-10-30 00:31:59 +0900

    ISR revalidate60

--- app/news/layout.tsx
+++ app/news/layout.tsx
@@ -1,9 +1,11 @@
-import Hero from '@/app/_components/Hero';
-import Sheet from '@/app/_components/Sheet';
+import Hero from "@/app/_components/Hero";
+import Sheet from "@/app/_components/Sheet";
 
 type Props = {
   children: React.ReactNode;
 };
+
+export const revalidate = 60;
 
 export default function NewsLayout({ children }: Props) {
   return (
